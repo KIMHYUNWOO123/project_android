b8a952292d7c807059766a850b4a27b76f1bcaf3
khw <wry1004@naver.com>
2022-04-22 17:35:51 +0900

    splash

---
+++ app/src/main/java/com/example/project_application/Splash.kt
@@ -0,0 +1,29 @@
+package com.example.project_application
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_splash2.*
+
+class splash_activity : AppCompatActivity() {
+
+    // This is the loading time of the splash screen
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+        Glide.with(this).load(R.raw.loading).into(imageView);
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
