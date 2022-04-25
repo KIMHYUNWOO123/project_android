89918f8b52ce9427400df6bf5a75251ef7970286
telemain1@hotmail.com <telemain1@hotmail.com>
2022-04-25 09:45:19 +0900

    edited by Danhee

--- app/src/main/java/com/example/project_application/Led2.kt
+++ app/src/main/java/com/example/project_application/Led2.kt
@@ -5,11 +5,17 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_led2.*
+import kotlinx.android.synthetic.main.activity_led2.kitchenLed
+import kotlinx.android.synthetic.main.activity_led2.livingLED
+import kotlinx.android.synthetic.main.activity_led2.myroonLed
 import org.eclipse.paho.client.mqttv3.MqttMessage
 import org.eclipse.paho.client.mqttv3.internal.wire.MqttReceivedMessage
 
 const val SUB_TOPIC = "iot/#"
 const val PUB_TOPIC2 = "iot/light"
+const val TOPIC_MYROOM = "iot/light/myRoom"
+const val TOPIC_KITCHEN = "iot/light/kitchen"
+const val TOPIC_LIVING = "iot/light/livingRoom"
 const val SERVICE_URI = "tcp://172.30.1.33"
 
 class Led2 : AppCompatActivity() {
@@ -42,6 +48,31 @@ class Led2 : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
             }
         })
+
+        livingLED.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked){
+                mqttClient.publish(TOPIC_LIVING, "livingroom turn on")
+            }else{
+                mqttClient.publish(TOPIC_LIVING, "livingroom turn off")
+            }
+        }
+
+        kitchenLed.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked){
+                mqttClient.publish(TOPIC_KITCHEN, "kitchen turn on")
+            }else{
+                mqttClient.publish(TOPIC_KITCHEN, "kitchen turn off")
+            }
+        }
+
+
+        myroonLed.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked){
+                mqttClient.publish(TOPIC_MYROOM, "myroom turn on")
+            }else{
+                mqttClient.publish(TOPIC_MYROOM, "myroom turn off")
+            }
+        }
     }
 
     fun onReceived(topic:String, message: MqttMessage) {
